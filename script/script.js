484015528f6e3169c6894ab7566e07b3fa8e0f0b
Darlan Mendes <darlan.mendes.estudo@gmail.com>
2022-07-16 16:21:02 -0300

    First commit. The calculater stylesheet is done and script is getting the subject clicked on which buttons.

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// variables set up-----------
+var display = document.getElementById("display");
+
+
+// selection of subject-------------
+const numberSelected =(issue)=>{
+    display.innerHTML =display.innerHTML+issue;
+    console.log(display.innerHTML)
+}
