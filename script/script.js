2020f1cb07273afd118e2408ceed088912aad233
Darlan Mendes <darlan.mendes.estudo@gmail.com>
2022-07-23 11:55:14 -0300

    adição da parte lógica (js)

--- script/script.js
+++ script/script.js
@@ -5,5 +5,17 @@ var display = document.getElementById("display");
 // selection of subject-------------
 const numberSelected =(issue)=>{
     display.innerHTML =display.innerHTML+issue;
-    console.log(display.innerHTML)
+    console.log(display.innerHTML);
+}
+const calculate =()=>{
+    let displayContentReplaced = display.innerHTML.replace("X","*");
+    let calculated = eval(displayContentReplaced);
+    display.innerHTML=calculated.toFixed(4)
+}
+const calculateSQRT =()=>{
+    let SQRTcalculatedDisplay =Math.sqrt(eval(display.innerHTML));
+    display.innerHTML = SQRTcalculatedDisplay.toFixed(6);
+} 
+const calculatePercent=()=>{
+    
 }
